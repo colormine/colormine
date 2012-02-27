76dbdee1d56ab1f4c91f3b9adc9181d41749b488
THEjoezack <me@joezack.com>
2012-02-26 22:34:39 -0500

    Updating complement demo to reduce servlet calls

--- colorMineServlet/colorMineServlet/www/js/complement.js
+++ colorMineServlet/colorMineServlet/www/js/complement.js
@@ -1,47 +1,48 @@
 $(document).ready(function() {
 
-		var colorBlock = '#color1'
-		var ComplementBlock = '#color2'
+	var colorBlock = $('#color1');
+	var complementBlock = $('#color2');
 
-		var Triad1 = "#color3"
-		var Triad2 = "#color4"
+	var triad1 = $('#color3');
+	var triad2 = $('#color4');
 
-		function normalize(color) {
+	function normalize(color) {
 		return parseInt(color.substring(1,3), 16) + '-' +
 			parseInt(color.substring(3,5), 16) + '-' +
 			parseInt(color.substring(5), 16);
+	}
+
+	function rgbToHex(color) {
+		rgb = color.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+		function hex(x) {
+			return ("0" + parseInt(x).toString(16)).slice(-2);
 		}
+		return normalize("#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]));
+	}
 
+	function setComplementColorBlock(color) {
+		complementBlock.css('background-color',color.color);
+	}
 
-		function setComplementColorBlock(color) {
-			$(ComplementBlock).css('background-color',color.color);
-		}
+	function setTriadBlocks(color) {
+		triad1.css('background-color',color.color[0]);
+		triad2.css('background-color',color.color[1]);
+	}
 
-		function setTriadBlocks(color) {
-			$(Triad1).css('background-color',color.color[0]);
-			$(Triad2).css('background-color',color.color[1]);
-		}
+	function setColorBlocks(color) {
+		colorBlock.css('background-color', color);
+	}
+	
+	function updateColorBlocks() {
+		var color = colorBlock.css('background-color');
+		var data = {
+			color: rgbToHex(color),
+		};
 
-		function setColorblocks(color) {
-			$(colorBlock).css('background-color',color);
+		$.get('/ColorMine/ComplementMath', data, setComplementColorBlock);
+		$.get('/ColorMine/TriadMath', data, setTriadBlocks);
+	}
 
-			var data = {
-				color: normalize(color),
-				};
-
-			var test =$.get('/ColorMine/ComplementMath',data,function(value){
-			setComplementColorBlock(value)});
-
-			var data = {
-				color: normalize(color),
-				};
-
-			var test =$.get('/ColorMine/TriadMath',data,function(value){
-			setTriadBlocks(value)});
-
-		}
-
-		
-
-	$('#colorPickerA').farbtastic(function(color) { setColorblocks(color); });
+	$('#colorPickerA').farbtastic(setColorBlocks);
+	$('#update').bind('click', updateColorBlocks);
 });
